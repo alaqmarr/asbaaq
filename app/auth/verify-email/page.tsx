07bd45d3b272e4cb21ca9c2631d35b1e57b181f6
Al Aqmar <alaqmarak0810@gmail.com>
2024-11-27 19:16:35 +0530

    mallicious code

--- app/auth/verify-email/page.tsx
+++ app/auth/verify-email/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const EmailVerification = ({params} : {params : {}}) => {
+const EmailVerification = () => {
   return (
     <div>
       <h1>Email Verification Page</h1>
